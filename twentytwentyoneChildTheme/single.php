b96e38d13a65429bbf7ec07ab1e9fa145bf99d04
NicolasPrimo <nicolas@molengeek.com>
2021-07-26 02:25:55 +0200

    4. Création du fichier single.php, c'est le template des pages d'article

---
+++ twentytwentyoneChildTheme/single.php
@@ -0,0 +1,5 @@
+<!-- 
+Toutes les style de page dans un theme ont un fichier qui sert de template pour savoir comment designer la page. 
+Le fichier single.php est le fichier qui sert de template aux articles, en créant ce fichier dans mon theme enfant, j'ai écrasé le fichier single.php du theme parent, ce qui a pour conséquence d'afficher une page blanche quand je vais regarder un article dans mon site, étant donné que ce fichier là est vide.
+Il faudra par la suite afficher le contenu que l'on désire dans ce fichier, comme le titre de l'article que l'ont veut voir , le texte etc...
+-->
