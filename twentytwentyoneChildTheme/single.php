4a36da71f5cc033fda1c37bbe56386dfd76e9419
nicolasprimo <nicolas@molengeek.com>
2021-07-26 17:01:28 +0200

    10. Affichage de l'image et de la date d'un post

--- twentytwentyoneChildTheme/single.php
+++ twentytwentyoneChildTheme/single.php
@@ -3,9 +3,14 @@
 <div class="container">
     <div class="post-border-right" id="post-float-left">
         <h2 class="post-title"><?php  the_title() ?></h2>
+        <!-- Affichage de l'image mise en avant sur un article, verifiez bien que vous en ayez mise une sur votre article ! -->
+        <?php the_post_thumbnail()
+        ?>
         <span class="post-author"><?php the_author_meta('display_name', 1); ?></span>
     </div>
     <div id="post-float-right">
+        <!-- Affichage de la date de l'article, la différence entre une methode qui commence par get_ ou non se situe dans le fait que devant une methode démarrant par get_ il faut écrire echo, "echo" sert en php a écrire des choses -->
+       <span class="post-date"> <?php echo get_the_date() ?></span>
         <?php the_content() ?>
     </div>
 </div>
