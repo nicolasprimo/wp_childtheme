d528e954d5053cb6c37a87c3f322e0638f908e14
NicolasPrimo <nicolas@molengeek.com>
2021-07-26 02:43:21 +0200

    6. Récupération du titre de l'article et affichage de celui ci dans la page single.php

--- twentytwentyoneChildTheme/single.php
+++ twentytwentyoneChildTheme/single.php
@@ -2,6 +2,8 @@
 https://developer.wordpress.org/reference/functions/get_header/ -->
 <?php get_header() ?>
 
+<!-- Utilisation de la function the_title() elle permet de recuperer le titre d'un article si bien sur on est sur la page web d'un article-->
+<h2><?php the_title() ?></h2>
 Il n'y a pour le moment pas le contenu de l'article que l'on essaie de voir :(
 
 <!-- Pareil que get_header() mais pour le footer -->
