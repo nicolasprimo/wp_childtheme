135ff816e88720895e0b4b30d9a58c6e66cbf6bb
nicolasprimo <nicolas@molengeek.com>
2021-07-26 16:49:16 +0200

    9. Suite du design de la page du post

--- twentytwentyoneChildTheme/single.php
+++ twentytwentyoneChildTheme/single.php
@@ -1,16 +1,16 @@
 <?php get_header() ?>
 
+<div class="container">
+    <div class="post-border-right" id="post-float-left">
+        <h2 class="post-title"><?php  the_title() ?></h2>
+        <span class="post-author"><?php the_author_meta('display_name', 1); ?></span>
+    </div>
+    <div id="post-float-right">
+        <?php the_content() ?>
+    </div>
+</div>
 
-<h2 class="post-title"><?php the_title() ?></h2>
-
-
-<?php the_content() ?>
-
-<!-- the_author_meta('display_name', 1) va nous permetre d'afficher des informations d'un utilisateur, il attend deux parametre le premier c'est quelle information ? pour le coup on veut le nom de l'utilisateur et le second c'est quel est l'id dans la DB de cet user pour savoir le quel séléctionné
-the_author_meta( 'display_name', $userID )
-Ceci n'est pas une super façon de faire, car si l'on va voir l'article d'un autre auteur on aura le mauvais qui s'affiche, nous verrons plus tard comment faire ca de manière plus optimisée.
--->
-<?php the_author_meta('display_name', 1); ?>
 
 
 <?php get_footer() ?>
+
