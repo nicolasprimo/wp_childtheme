e4f87e38f1fe10651047178532fa06cb4595299c
NicolasPrimo <nicolas@molengeek.com>
2021-07-26 03:04:41 +0200

    7. Affichage du contenu et de l'auteur de l'article

--- twentytwentyoneChildTheme/single.php
+++ twentytwentyoneChildTheme/single.php
@@ -1,10 +1,16 @@
-<!-- On fait appel a la function get_header(), qui est une function prédéfinie de wordpress pour injecter le header là ou elle est appelée
-https://developer.wordpress.org/reference/functions/get_header/ -->
 <?php get_header() ?>
 
-<!-- Utilisation de la function the_title() elle permet de recuperer le titre d'un article si bien sur on est sur la page web d'un article-->
-<h2><?php the_title() ?></h2>
-Il n'y a pour le moment pas le contenu de l'article que l'on essaie de voir :(
 
-<!-- Pareil que get_header() mais pour le footer -->
+<h2><?php the_title() ?></h2>
+
+
+<?php the_content() ?>
+
+<!-- the_author_meta('display_name', 1) va nous permetre d'afficher des informations d'un utilisateur, il attend deux parametre le premier c'est quelle information ? pour le coup on veut le nom de l'utilisateur et le second c'est quel est l'id dans la DB de cet user pour savoir le quel séléctionné
+the_author_meta( 'display_name', $userID )
+Ceci n'est pas une super façon de faire, car si l'on va voir l'article d'un autre auteur on aura le mauvais qui s'affiche, nous verrons plus tard comment faire ca de manière plus optimisée.
+-->
+<?php the_author_meta('display_name', 1); ?>
+
+
 <?php get_footer() ?>
