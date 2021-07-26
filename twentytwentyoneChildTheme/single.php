4f43e7f53b5fea830f3bd7a48b93275d908d4e26
NicolasPrimo <nicolas@molengeek.com>
2021-07-26 02:34:00 +0200

    5. Ajout du header et footer du theme parent dans la page single.php de notre theme enfant afin davoir le style du theme parent

--- twentytwentyoneChildTheme/single.php
+++ twentytwentyoneChildTheme/single.php
@@ -1,5 +1,8 @@
-<!-- 
-Toutes les style de page dans un theme ont un fichier qui sert de template pour savoir comment designer la page. 
-Le fichier single.php est le fichier qui sert de template aux articles, en créant ce fichier dans mon theme enfant, j'ai écrasé le fichier single.php du theme parent, ce qui a pour conséquence d'afficher une page blanche quand je vais regarder un article dans mon site, étant donné que ce fichier là est vide.
-Il faudra par la suite afficher le contenu que l'on désire dans ce fichier, comme le titre de l'article que l'ont veut voir , le texte etc...
--->
+<!-- On fait appel a la function get_header(), qui est une function prédéfinie de wordpress pour injecter le header là ou elle est appelée
+https://developer.wordpress.org/reference/functions/get_header/ -->
+<?php get_header() ?>
+
+Il n'y a pour le moment pas le contenu de l'article que l'on essaie de voir :(
+
+<!-- Pareil que get_header() mais pour le footer -->
+<?php get_footer() ?>
